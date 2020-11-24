a83d14856b8d18b209e4a91dbb93eb2d0fd27662
pillemer <pillemer@gmail.com>
2020-11-24 10:09:24 +1000

    reconfiguring project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 import lodash from 'lodash';
+import format from 'date-fns';
 
-const TodoItem = ( title, description, priority, dueDate,group='default' ) => {
-    const sayHello = (somethng) => console.log(lodash.capitalize('hello ' +somethng));
-    return { title, description, priority, group, dueDate, sayHello };
+// change for visual verificaton of update
+console.log('trying...1');
+
+class ListItem {
+    constrcutor(title) {
+        this.title = title;
+        this.complete = false;
+    }
+
+    toggleComplete() {
+        this.complete = !this.complete;
+    }
+
+    isDone() {
+        let done = ''
+        if (this.complete){
+            done = 'Done';
+        } else {  done = 'Not Done'}
+        console.log(`${this.title} is ${done}`)
+    }
 }
 
-const entry1 = TodoItem('first entry', 'create to do list', 'high', 'tomorrow')
-entry1.sayHello(entry1.group);
+let item1 = new ListItem('first time')
+console.log(item1)
+
+item1.isDone()
+console.log('toggling..')
+item1.toggleComplete()
+item1.isDone()
